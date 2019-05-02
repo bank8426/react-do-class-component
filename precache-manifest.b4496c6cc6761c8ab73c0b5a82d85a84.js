69785e97354c79f7ea7c9795bf254ca2d8c8c311
bank8426 <bank_55555@hotmail.com>
2019-05-02 10:08:01 +0700

    Updates

---
+++ precache-manifest.b4496c6cc6761c8ab73c0b5a82d85a84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972cfa6b80b5385562cb3853fc1f542b",
+    "url": "/react-do-class-component/index.html"
+  },
+  {
+    "revision": "ae626360fc1fa9216f25",
+    "url": "/react-do-class-component/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-do-class-component/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ae626360fc1fa9216f25",
+    "url": "/react-do-class-component/static/js/main.04d8e9a5.chunk.js"
+  },
+  {
+    "revision": "021199947669cd922a8b",
+    "url": "/react-do-class-component/static/js/runtime~main.b8c61345.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-do-class-component/static/media/logo.5d5d9eef.svg"
+  }
+]);
